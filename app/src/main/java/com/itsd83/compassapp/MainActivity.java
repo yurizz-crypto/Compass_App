72b198098f17ddbceae24d7070f0b2744c8ef8f0
yurizz-crypto <pacaljoenel08@gmail.com>
2025-10-16 11:30:23 +0800

    Compass App

---
+++ app/src/main/java/com/itsd83/compassapp/MainActivity.java
@@ -0,0 +1,111 @@
+package com.itsd83.compassapp;
+
+import android.annotation.SuppressLint;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager compassSensorManager;
+    private Sensor accelerometer;
+    private Sensor magnetometer;
+
+    private TextView tv_degrees;
+    private ImageView iv_compass;
+
+    private final float[] accelerometerReading = new float[3];
+    private final float[] magnetometerReading = new float[3];
+
+    private final float[] rotationMatrix = new float[9];
+    private final float[] orientationAngles = new float[3];
+
+    private float current_degree = 0f;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv_degrees = findViewById(R.id.degrees);
+        iv_compass = findViewById(R.id.compass_image);
+
+        compassSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = compassSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetometer = compassSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (accelerometer != null) {
+            compassSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+        }
+        if (magnetometer != null) {
+            compassSensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        compassSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, accelerometerReading, 0, accelerometerReading.length);
+        } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, magnetometerReading, 0, magnetometerReading.length);
+        }
+
+        updateOrientationAngles();
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void updateOrientationAngles() {
+        boolean success = SensorManager.getRotationMatrix(rotationMatrix, null, accelerometerReading, magnetometerReading);
+
+        if (success) {
+            SensorManager.getOrientation(rotationMatrix, orientationAngles);
+
+
+            float degrees = (float) Math.toDegrees(orientationAngles[0]);
+
+            if (degrees < 0) {
+                degrees += 360;
+            }
+            int degreesInt = Math.round(degrees);
+
+            tv_degrees.setText(degreesInt + "\u00B0");
+
+            RotateAnimation rotate = new RotateAnimation(
+                    current_degree,
+                    -degrees,
+                    Animation.RELATIVE_TO_SELF, 0.5f,
+                    Animation.RELATIVE_TO_SELF, 0.5f
+            );
+
+            rotate.setDuration(250);
+            rotate.setFillAfter(true);
+
+            iv_compass.startAnimation(rotate);
+            current_degree = -degrees;
+        }
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // You can handle changes in sensor accuracy here if needed, but it's not required for this app.
+    }
+}
